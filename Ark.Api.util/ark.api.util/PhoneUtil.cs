10f39b7c1ab5ff16e3522eba2ea9dcba954f8ffd
Immanuel R <raj@immanuel.co>
2025-09-22 11:45:02 +0530

    more cahnges.

---
+++ Ark.Api.util/ark.api.util/PhoneUtil.cs
@@ -0,0 +1,16 @@
+﻿namespace ark.net.util
+{
+    public class PhoneUtil
+    {
+
+    }
+    public static class PhoneExtn
+    {
+        public static bool IsValidPhone(this string? val)
+        {
+            if (val.AnyNull()) return false;
+            if (val.Length < 10) return false;
+            return true;
+        }
+    }
+}
